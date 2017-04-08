b7be470cc23294488917b7adff90108c80d2f9ef
Hari Prasad <hariprasad.small@gmail.com>
2017-04-08 14:13:23 -0700

    Updated notification

--- service-worker.js
+++ service-worker.js
@@ -30,7 +30,7 @@ self.addEventListener('notificationclick', function(event) {
   }).then(function(clientList) {
     for (var i = 0; i < clientList.length; i++) {
       var client = clientList[i];
-      if (client.url === '/' && 'focus' in client) {
+      if (client.url === 'http://google.com' && 'focus' in client) {
         return client.focus();
       }
     }
